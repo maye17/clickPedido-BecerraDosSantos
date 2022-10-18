2d0d7f7187d0fb00aec3b96d7f77b3ff41e8b73e
maye17 <mayerlin.dossantos@gmail.com>
2022-10-18 14:41:21 -0300

    Creando proyecto clickPedido

---
+++ index.js
@@ -0,0 +1,11 @@
+//Definición de variables
+/* let direction ="Puerto Madero 366 - Buenos Aires"; */
+
+const precioBife =2500;
+const precioBrocheta =1800;
+const precioChurrasco = 2000;
+let total =0;
+let ErrorPedido =false;
+
+//Proceso
+
